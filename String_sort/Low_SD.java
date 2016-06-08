3c8091cf94fccda7172331c07a601a74ceaed041
zhou_chenz <zhou_chenz@163.com>
2016-06-08 21:32:34 +0800

    add String sort

---
+++ String_sort/Low_SD.java
@@ -0,0 +1,49 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Date;
+import edu.princeton.cs.algs4.Stopwatch;
+import java.util.*;
+
+
+public class Low_SD {
+	private Low_SD() { }
+	public static void sort(String[] a, int w) {
+		int n = a.length;
+		int list_sz = 256;
+		String[] aux = new String[n];
+		for(int d = w - 1; d >= 0; d--){
+			//stat from low char
+			int [] count = new int[list_sz + 1];
+			for(int i = 0; i < n; i++)
+				// count freq
+				count[a[i].charAt(d) + 1]++ ;
+			for(int r = 0; r < list_sz; r++)
+				//change count[] to index list
+				count[r+1] += count[r];
+			
+            for (int i = 0; i < n; i++)
+                aux[count[a[i].charAt(d)]++] = a[i];
+
+			for(int j = 0; j < n; j++)
+				a[j] = aux[j];
+		}
+
+
+		
+	
+    }
+	
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+		int N = a.length;
+		int w = a[0].length();
+		Low_SD.sort(a, w);
+		for(int i = 0; i < N; i++)
+			StdOut.println(a[i]);
+
+    }
+	
+}
+
+
---
+++ String_sort/Mag_SD.java
@@ -0,0 +1,63 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Date;
+import edu.princeton.cs.algs4.Stopwatch;
+import java.util.*;
+
+
+public class Mag_SD {
+	private static int Collect_sz = 256;
+	private static String [] aux;
+
+	private Mag_SD() { }
+
+	private static int charAt(String s, int d){
+		if(d < s.length())
+			return s.charAt(d);
+		else
+			return -1;
+
+	}
+	public static void sort(String[] a) {
+		int n = a.length;
+		aux = new String[n];
+		sort(a, 0, n-1, 0);
+	}
+		
+	private static void sort(String[] a, int lo, int hi, int d){
+		if(hi <= lo)
+			return;
+		int [] count = new int[Collect_sz + 2];
+		for(int i = lo; i <= hi; i++)
+			//stat from magnificant char
+			count[charAt(a[i], d) + 2]++ ;
+
+		for(int r = 0; r < (Collect_sz+1); r++){
+				//change count[] to index list
+				count[r+1] += count[r];
+				//StdOut.println(count[r]);
+		}
+
+		 for (int i = lo; i <= hi; i++)
+                aux[count[charAt(a[i], d) + 1]++] = a[i];
+		
+		for(int j = lo; j <= hi; j++)
+				a[j] = aux[j - lo];
+
+		for(int r = 0; r < Collect_sz; r++)
+			sort(a, lo + count[r], lo + count[r+1] - 1, d+1);
+		
+	}
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+		int N = a.length;
+		Mag_SD.sort(a);
+		for(int i = 0; i < N; i++)
+			StdOut.println(a[i]);
+
+    }
+	
+}
+
+
